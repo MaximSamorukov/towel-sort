6ec352d2fed78e72aa4b8d24bdb6e96e90285891
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-09-08 22:39:38 +0300

    init: create app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+  return matrix.map((item, index) => {
+    if (index === 0 || index % 2 === 0) {
+      return item.sort((a, b) => 1);
+    } else {
+      return item.sort((a, b) => -1);
+    }
+  }).flat();
 }
